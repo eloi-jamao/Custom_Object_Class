6ac2bd2736d49f4286ef17d0448a52916b8fee35
eloi-jamao <53044991+eloi-jamao@users.noreply.github.com>
2020-02-04 20:04:31 +0100

    Update train_torch.py

--- train_torch.py
+++ train_torch.py
@@ -5,6 +5,7 @@ import torch.optim as optim
 import numpy as np
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
+from torchvision.models import resnet50
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import os
@@ -12,16 +13,20 @@ import argparse
 
 root = os.getcwd()
 images_dir = root + '/images'
+num_classes = len(os.listdir(images_dir)))
+
 if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.cuda.current_device()
+input_size = (224,224,3)
 
 transform = transforms.Compose([
                                 transforms.Resize(150), # Resize the short side of the image to 150 keeping aspect ratio
                                 transforms.CenterCrop(150), # Crop a square in the center of the image
                                 transforms.ToTensor(), # Convert the image to a tensor with pixels in the range [0, 1]
                                 ])
+
 def train_model(model, optimizer, criterion, train_loader, epochs):
 
     for epoch in range(epochs):
@@ -49,10 +54,18 @@ if __name__ == '__main__':
     parser.add_argument('-b', '--batch_size', type=int, default=5, help='Batch size')
     parser.add_argument('-ld', '--logdir', default=(os.getcwd() + r'/tf_logs'), help='Location of saved tf.summary')
     args = parser.parse_args()
-
+    
+    model = resnet50(pretrained=True)
+    for param in model.parameters():
+        para.requires_grad = False
+    ftrs = model.fc.in_features
+    model.fc = nn.Linear(features, num_classes)
+    
     train_dataset = ImageFolder(images_dir, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
 
     optimizer = optim.Adam(model.parameters(), lr=0.001)
-    loss = nn.BCELoss()
+    loss = nn.CrossEntropyLoss()
     train_model(net, optimizer, loss, train_loader, args.num_epochs)
+    
+    
