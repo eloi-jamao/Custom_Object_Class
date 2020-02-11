f12058e1a144bc7d448d5e1c06d16dcf510533d8
Eloi Marimon <eloimarimonplastica@gmail.com>
2020-02-11 17:25:09 +0100

    adding testing for models

--- train_torch.py
+++ train_torch.py
@@ -14,18 +14,19 @@ from tqdm import tqdm
 
 root = os.getcwd()
 images_dir = root + '/images'
+test_dir = root + '/test'
 num_classes = len(os.listdir(images_dir))
-model_path = root + '/model_weights/weights0'
+model_path = root + f'/model_weights/weights' + str(len(os.listdir(root+'/model_weights')))
 input_size = (224,224,3)
 
 if torch.cuda.is_available():
     device = torch.device("cpu")
 else:
-    device = torch.cuda.current_device()
+    device = torch.device("cpu")
 
 transform = transforms.Compose([
-                                transforms.Resize(150), # Resize the short side of the image to 150 keeping aspect ratio
-                                transforms.CenterCrop(150), # Crop a square in the center of the image
+                                transforms.Resize(input_size[0]), # Resize the short side of the image to 150 keeping aspect ratio
+                                transforms.CenterCrop(input_size[0]), # Crop a square in the center of the image
                                 transforms.ToTensor(), # Convert the image to a tensor with pixels in the range [0, 1]
                                 ])
 
@@ -38,7 +39,7 @@ class custom_resnet(nn.Module):
 
           in_features = self.base_model.fc.in_features
           self.base_model.fc = nn.Linear(in_features, num_classes)
-
+          print(self.base_model)
 
     def forward(self, x):
         y = self.base_model(x)
@@ -62,27 +63,55 @@ def train_model(model, optimizer, criterion, train_loader, epochs):
             loss.backward()
             optimizer.step()
 
+def test_model(model,test_loader, epochs):
+
+    for epoch in range(epochs):
+        # test
+        acc = 0
+        model.eval()
+        test_loop = tqdm(test_loader, unit="batches")  # For printing the progress bar
+        with torch.no_grad():
+            for data, target in test_loop:
+                test_loop.set_description('[TEST] Epoch {}/{}'.format(epoch + 1, epochs))
+                data, target = data.float().to(device), target.to(device)
+                #target = target.unsqueeze(-1)
+                output = model(data)
+                #print(output.shape, target.shape)
+                acc += correct_predictions(output, target)
+        acc = 100. * acc / len(test_loader.dataset)
+        print(f'Test accuracy of {acc}')
+
+def correct_predictions(predicted_batch, label_batch):
+  pred = predicted_batch.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+  acum = pred.eq(label_batch.view_as(pred)).sum().item()
+  return acum
+
+def describe_dataset(dataset):
+    it=iter(dataset)
+    for image,label in it:
+        print(len(dataset))
+        print(label)
+        plt.imshow(image.T)
+        plt.show()
+        break
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pipeline execution')
     parser.add_argument('-o', '--objects', default=None, help='classes to train')
     parser.add_argument('-e', '--num_epochs', type=int, default=1, help='Number of epochs')
     parser.add_argument('-b', '--batch_size', type=int, default=5, help='Batch size')
+    parser.add_argument('-t', '--test', default=False, help='Test the model during training')
     parser.add_argument('-ld', '--logdir', default=(os.getcwd() + r'/tf_logs'), help='Location of saved tf.summary')
     args = parser.parse_args()
 
     train_dataset = ImageFolder(images_dir, transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    if args.test:
+        test_dataset = ImageFolder(test_dir, transform=transform)
+        test_loader = DataLoader(test_dataset, batch_size=1)
 
-    it=iter(train_loader)
-
-    for image,label in train_loader:
-        print(len(train_dataset))
-        print(label[0])
-        plt.imshow(image[0].permute(1,2,0))
-        plt.show()
-        break
-
+    #describe_dataset(train_dataset)
 
     loss = nn.CrossEntropyLoss()
     model = custom_resnet()
@@ -90,5 +119,7 @@ if __name__ == '__main__':
     loss = nn.CrossEntropyLoss()
 
     train_model(model, optimizer, loss, train_loader, args.num_epochs)
+    if args.test:
+        test_model(model, test_loader, args.num_epochs)
 
     torch.save(model.state_dict(), model_path)
